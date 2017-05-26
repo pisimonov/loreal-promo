ce913631deaefdc9b6a122a1d0017f224c8d815c
Eugene Yemelin <EEmelin@detmir.ru>
2017-05-26 09:53:25 +0300

    Initial upload

---
+++ js/pl-main.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.pl-gallery').slick({
+    dots: true,
+    infinite: true,
+    adaptive: true
+  });
+
+  $('.pl-content-slider').slick({
+    infinite: false,
+    arrows: false,
+    dots: true,
+    adaptive: true
+  });
+})
